fac32e022c6cee88699c2234c4b5bf1ec70c14e4
Dan <inb3zna@gmail.com>
2023-12-02 11:36:03 +0200

    project setup

---
+++ src/test/java/com/teamrocket/tms/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamrocket.tms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
