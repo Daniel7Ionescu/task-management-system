09033945404e1ad6f677d31af67f2aab8d6d6db8
Andrei <mirceanghel98@gmail.com>
2023-12-03 16:57:24 +0200

    Add Task Controller and Create Task

--- src/main/java/com/teamrocket/tms/services/TaskServiceImpl.java
+++ src/main/java/com/teamrocket/tms/services/TaskServiceImpl.java
@@ -3,9 +3,11 @@ package com.teamrocket.tms.services;
 import com.teamrocket.tms.models.dtos.TaskDTO;
 import com.teamrocket.tms.models.entities.Task;
 import com.teamrocket.tms.repositories.TaskRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class TaskServiceImpl implements TaskService {
 
@@ -20,8 +22,8 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public TaskDTO createTask(TaskDTO taskDTO) {
         Task taskEntity = modelMapper.map(taskDTO, Task.class);
-
         Task savedTaskEntity = taskRepository.save(taskEntity);
+        log.info("Task {} : {} inserted in db.", taskEntity.getId(), taskEntity.getTitle());
 
         return modelMapper.map(savedTaskEntity, TaskDTO.class);
     }
