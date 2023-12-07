fafcc311d7cb4def90e01e4d05b4ea60b95099ac
Andrei <mirceanghel98@gmail.com>
2023-12-07 19:16:37 +0200

    Added API for users to be able to change their personal info. Also, groupped the UserAPI's in the User controller. There was one for user, one for task, then again one for user :)) Just changed their oreder.

--- src/main/java/com/teamrocket/tms/services/user/UserServiceImpl.java
+++ src/main/java/com/teamrocket/tms/services/user/UserServiceImpl.java
@@ -68,6 +68,19 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public UserDTO updateUser(Long userId, UserDTO userDTO) {
+        userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User with id " + userId + " not found."));
+
+        User user = modelMapper.map(userDTO, User.class);
+        user.setId(userId);
+
+        User savedUser = userRepository.save(user);
+        log.info("User {} : {} updated in db", savedUser.getId(), savedUser.getLastName());
+
+        return modelMapper.map(savedUser, UserDTO.class);
+    }
+
+    @Override
     public TaskDTO createTask(TaskDTO taskDTO, long id) {
         User userEntity = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User with id " + id + " not found."));
       
