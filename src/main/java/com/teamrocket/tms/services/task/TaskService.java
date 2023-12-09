b96b47ac6e3a9d3cbf4cbf21b17b82d4ec9744db
Gabriella Nuca <gabriellanuca@gmail.com>
2023-12-09 13:47:35 +0200

    refactored userController

--- src/main/java/com/teamrocket/tms/services/task/TaskService.java
+++ src/main/java/com/teamrocket/tms/services/task/TaskService.java
@@ -2,13 +2,12 @@ package com.teamrocket.tms.services.task;
 
 import com.teamrocket.tms.models.dtos.TaskDTO;
 import com.teamrocket.tms.models.entities.Task;
-import com.teamrocket.tms.models.entities.User;
 
 import java.util.List;
 
 public interface TaskService {
 
-    TaskDTO createTask(TaskDTO taskDTO, User userEntity);
+    TaskDTO createTask(TaskDTO taskDTO, String userName);
 
     List<TaskDTO> getAllTasks();
 
